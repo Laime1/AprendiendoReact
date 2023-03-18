d6495ee9ea7c1937b03ce5b69840de3933751efd
Laime Antonio <anlaifel09@gmail.com>
2023-03-18 15:19:46 -0400

    Agregando diseño de libreria

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 
 import './App.css';
+import { Input, Button, Col, Row, message } from 'antd';
+
 
 function App() {
   return (
     <div >
-      <input type="text" name="username" placeholder="Enter username"/>
-      <input type="password" name="password" placeholder="Enter password"/> 
-      <button onClick={()=>alert('Login Successful!')}>Login</button>
+      <input type="text" name="username" placeholder="Enter username" />
+      <input type="password" name="password" placeholder="Enter password" />
+      <button onClick={() => alert('Login Successful!')}>Login</button>
+      <Row>
+        <Col span="6" offset="8">
+          <Input name="username" placeholder="Enter username" />
+          <Input.Password name="username" placeholder="Enter username" />
+          <Button type="primary" onClick={() => message.info("Login")}>Login</Button>
+        </Col>
+      </Row>
       <hr></hr>
     </div>
   );
